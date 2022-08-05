e6090bdd8461817c39b291f202d6640997caf65d
Ricardo Paschoali <ricardo.paschoali@assaabloy.com>
2022-08-05 17:40:48 -0300

    poo ainda bugado

--- lib/rgb_led_cpp/rgb_led.cpp
+++ lib/rgb_led_cpp/rgb_led.cpp
@@ -1,4 +1,10 @@
-#include "rgb_led.h"
+#include "rgb_led.hpp"
+
+#define LEDC_TIMER LEDC_TIMER_2
+#define LEDC_MODE LED
+#define LEDC_OUTPUT_IO (5)              // Define the output GPIO
+#define LEDC_DUTY_RES LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
+#define LEDC_FREQUENCY (10)             // Frequency in Hertz. Set frequency at 5 kHz
 
 namespace LED
 {
@@ -8,33 +14,33 @@ namespace LED
         // red
         ledc_ch[0].channel = LEDC_CHANNEL_0;
         ledc_ch[0].gpio = rgb_led_red_gpio;
-        ledc_ch[0].mode = LEDC_HIGH_SPEED_MODE;
-        ledc_ch[0].timer_index = LEDC_TIMER_0;
+        ledc_ch[0].mode = LEDC_MODE;
+        ledc_ch[0].timer_index = LEDC_TIMER;
 
         // green
 
         ledc_ch[1].channel = LEDC_CHANNEL_1;
         ledc_ch[1].gpio = rgb_led_green_gpio;
-        ledc_ch[1].mode = LEDC_HIGH_SPEED_MODE;
-        ledc_ch[1].timer_index = LEDC_TIMER_0;
+        ledc_ch[1].mode = LEDC_MODE;
+        ledc_ch[1].timer_index = LEDC_TIMER;
 
         // blue
 
         ledc_ch[2].channel = LEDC_CHANNEL_2;
         ledc_ch[2].gpio = rgb_led_blue_gpio;
-        ledc_ch[2].mode = LEDC_HIGH_SPEED_MODE;
-        ledc_ch[2].timer_index = LEDC_TIMER_0;
+        ledc_ch[2].mode = LEDC_MODE;
+        ledc_ch[2].timer_index = LEDC_TIMER;
 
         // configure timer zero
         ledc_timer_config_t ledc_timer;
 
-        ledc_timer.duty_resolution = LEDC_TIMER_8_BIT;
-        ledc_timer.freq_hz = 100;
-        ledc_timer.speed_mode = LEDC_HIGH_SPEED_MODE;
-        ledc_timer.timer_num = LEDC_TIMER_0;
-        ledc_timer.clk_cfg = LEDC_USE_REF_TICK;
+        ledc_timer.duty_resolution = LEDC_DUTY_RES;
+        ledc_timer.freq_hz = LEDC_FREQUENCY;
+        ledc_timer.speed_mode = LEDC_MODE;
+        ledc_timer.timer_num = LEDC_TIMER;
+        ledc_timer.clk_cfg = LEDC_AUTO_CLK;
 
-        ledc_timer_config(&ledc_timer);
+        ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
 
         printf("\n");
         printf("0-> channel:%d gpio:%d mode:%d timer_index:%d \n",
@@ -67,7 +73,7 @@ namespace LED
             ledc_channel.intr_type = LEDC_INTR_DISABLE;
             ledc_channel.speed_mode = ledc_ch[rgb_ch].mode;
             ledc_channel.timer_sel = ledc_ch[rgb_ch].timer_index;
-            ledc_channel_config(&ledc_channel);
+            ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
 
             printf("rgb_ch:%d-> gpio_num:%d speed_mode:%d timer_sel:%d channel:%d\n",
                    rgb_ch, ledc_channel.gpio_num, ledc_channel.speed_mode, ledc_channel.timer_sel, ledc_channel.channel);
@@ -85,8 +91,8 @@ namespace LED
         for (int rgb_ch = 0; rgb_ch < RGB_LED_CHANNEL_NUM; rgb_ch++)
         {
 
-            ledc_set_duty(ledc_ch[rgb_ch].mode, ledc_ch[rgb_ch].channel, color[rgb_ch]);
-            ledc_update_duty(ledc_ch[rgb_ch].mode, ledc_ch[rgb_ch].channel);
+            ESP_ERROR_CHECK(ledc_set_duty(ledc_ch[rgb_ch].mode, ledc_ch[rgb_ch].channel, color[rgb_ch]));
+            ESP_ERROR_CHECK(ledc_update_duty(ledc_ch[rgb_ch].mode, ledc_ch[rgb_ch].channel));
 
             printf("mode:%d  channel:%d color:%d\n", ledc_ch[rgb_ch].mode, ledc_ch[rgb_ch].channel, color[rgb_ch]);
         }
