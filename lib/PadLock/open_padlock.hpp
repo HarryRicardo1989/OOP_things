e5c6f17934f8abd4148503aead93f4d65f4c981b
Ricardo Paschoali <ricardo.paschoali@assaabloy.com>
2022-08-19 13:12:20 -0300

    maybe

--- lib/PadLock/open_padlock.hpp
+++ lib/PadLock/open_padlock.hpp
@@ -1,12 +1,17 @@
 #pragma once
 
 #include "rgb_led.hpp"
+#include "GPIO/gpio.hpp"
 
 namespace PADLOCK
 {
     class OPEN_PADLOCK
     {
     private:
+        Perifericos::GPIO lockpad_close = Perifericos::GPIO();
+        Perifericos::GPIO lockpad_open;
+        Perifericos::GPIO lockpad_enable;
+
     public:
         OPEN_PADLOCK(int gpio);
         bool open_with_password(char password, int pass_length);
